16c161e4f389e2257bca2ad3605bc7ccb1902fa1
Eyobel Legese <eyobel7k@gmail.com>
2021-09-29 20:54:31 -0700

    updated app.js

--- App.js
+++ App.js
@@ -2,11 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+     
+      <View style={styles.blue}>
+        <Text>Header</Text>
+        
+        
+      </View>
+      <View style={styles.red}>
+        <Text>Expo project</Text>
+        
+        
+      </View>
+      <View style={styles.purple}>
+       <Text>Footer</Text>
+       
+      </View>
     </View>
   );
 }
@@ -14,8 +32,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection:'column',
+    backgroundColor: 'black',
+    
+    
+  },
+  blue: {
+    flex: 1,
+    flexDirection:'row',
+    width:'100%',
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: "space-evenly",
+    
+   
+    
+  },
+  red: {
+    flex: 8,
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  purple: {
+    flex: 1,
+    backgroundColor: 'purple',
+    alignItems: 'center'
+   
+  },
+ 
+
 });
